e36fdfa45791cfb27d1c936d10c94cbd14eac2bb
Yen Colon <yendejesusc41@gmail.com>
2020-04-20 07:42:13 -0400

    adding database seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,79 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        DB::table('categories')->insert([[
+            'id' => 1,
+            'name' => 'Pizza',
+            'description' => 'Pizzas all flavors'
+        ], [
+            'id' => 2,
+            'name' => 'Drinks',
+            'description' => 'All available drinks'
+        ]]);
+        
+        DB::table('products')->insert([
+            'id' => 1,
+            'name' => 'Margherita',
+            'description' => 'Tomato sauce, mozzarella, and oregano',
+            'price' => 10.00,
+            'image_url' => '',
+            'category_id' => 1,
+        ]);
+
+        DB::table('products')->insert([
+            'id' => 2,
+            'name' => 'Marinara',
+            'description' => 'Tomato sauce, garlic and basil',
+            'price' => 10.00,
+            'category_id' => 1,
+        ]);
+
+        DB::table('products')->insert([
+            'id' => 3,
+            'name' => 'Quattro Stagioni',
+            'description' => 'Tomato sauce, mozzarella, mushrooms, ham, artichokes, olives, and oregano',
+            'price' => 10.00,
+            'category_id' => 1,
+        ]);
+
+        DB::table('products')->insert([
+            'id' => 4,
+            'name' => 'Carbonara',
+            'description' => 'Tomato sauce, mozzarella, parmesan, eggs, and bacon',
+            'price' => 10.00,
+            'category_id' => 1,
+        ]);
+
+        DB::table('products')->insert([
+            'id' => 5,
+            'name' => 'Frutti di Mare',
+            'description' => 'Tomato sauce and seafood',
+            'price' => 10.00,
+            'category_id' => 1,
+        ]);
+
+        DB::table('products')->insert([
+            'id' => 6,
+            'name' => 'Quattro Formaggi',
+            'description' => 'Tomato sauce, mozzarella, parmesan, gorgonzola cheese, artichokes, and oregano',
+            'price' => 10.00,
+            'category_id' => 1,
+        ]);
+
+        DB::table('products')->insert([
+            'id' => 7,
+            'name' => 'Napoli',
+            'description' => 'Tomato sauce, mozzarella, oregano, anchovies',
+            'price' => 10.00,
+            'category_id' => 1,
+        ]);
+
+        DB::table('products')->insert([
+            'id' => 8,
+            'name' => 'Pizza al Pesto',
+            'description' => 'Tomato, mozzarella, Genoese pesto, pine nuts, and olives',
+            'price' => 10.00,
+            'category_id' => 1,
+        ]);
     }
 }
