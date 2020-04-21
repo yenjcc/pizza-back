9d0cce0809efefca56c6eb7c7f8516e7ba070cdb
Yen Colon <yendejesusc41@gmail.com>
2020-04-21 18:22:39 -0400

    adding purchase controller

---
+++ app/Http/Controllers/PurchaseController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreatePurchaseRequest;
+use App\Product;
+use App\Purchase;
+use App\PurchaseDetail;
+use Illuminate\Http\Request;
+
+class PurchaseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $purchases = Purchase::all();
+        
+        return response()->json([
+            "purchases" => $purchases
+        ], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreatePurchaseRequest $request)
+    {
+        $purchase = Purchase::create($request->all());
+
+        $productsBuyed = $request->products;
+
+        foreach($productsBuyed as $product){
+            PurchaseDetail::create([
+                'product_id' => $product['id'],
+                'quantity' => $product['quantity'], 
+                'price_at_moment_to_buy' => Product::find($product['id'])->price,
+                'purchase_id' => $purchase->id
+            ]);
+        }
+
+        return response()->json([
+            'purchase' => $purchase
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  integer  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Purchase $purchase)
+    {
+        return response()->json(['purchase' => $purchase], 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Purchase  $purchase
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Purchase $purchase)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Purchase  $purchase
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Purchase $purchase)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Purchase  $purchase
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Purchase $purchase)
+    {
+        //
+    }
+}
