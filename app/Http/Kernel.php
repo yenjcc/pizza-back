d42247cef76ec8b6b2b9f8e0fe462dd95ecb6379
Yen Colon <yendejesusc41@gmail.com>
2020-04-23 23:25:35 -0400

    cors configuration

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        
     ];
 
     /**
@@ -43,6 +42,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Fruitcake\Cors\HandleCors::class,
         ],
     ];
 
