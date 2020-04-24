c51150c8e65cb913f5e736b2d17d4945ee4f1716
Yen Colon <yendejesusc41@gmail.com>
2020-04-24 00:10:27 -0400

    enabling cors

--- config/app.php
+++ config/app.php
@@ -162,7 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Fruitcake\Cors\CorsServiceProvider::class, 
-        Barryvdh\Cors\ServiceProvider::class,
+      
         /*
          * Package Service Providers...
          */
